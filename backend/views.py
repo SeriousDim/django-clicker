620548833b1f524ac0415f6c28b6a7b7d8c0cdfe
SeriousDim <dlykov2001@yandex.ru>
2022-05-25 14:46:31 +0500

    3.5 Базовый функционал кликера (some frontend fixes)

--- backend/views.py
+++ backend/views.py
@@ -11,39 +11,6 @@ from backend.forms import UserForm
 from backend.models import Core, Boost
 from backend.serializers import CoreSerializer, BoostSerializer
 
-"""
-def index(request):
-    return HttpResponse("Здесь будет Майк Вазовски")
-
-
-def register(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')
-
-        return render(request, 'register.html', {'form': form})
-
-    form = UserForm()
-    return render(request, 'register.html', {'form': form})
-
-
-def user_login(request):
-    form = UserForm()
-
-    if request.method == 'POST':
-        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-        if user:
-            login(request, user)
-            return redirect('index')
-
-        return render(request, 'login.html', {'form': form, 'invalid': True})
-
-    return render(request, 'login.html', {'form': form})
-"""
-
 
 class Register(APIView):
     def get(self, request):
